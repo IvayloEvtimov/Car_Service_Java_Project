66bf1faf223126727e7a580c968cc65c72675ae3
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-10-24 21:50:58 +0300

    Garage.java: class for entity table Garage

---
+++ src/main/java/com/project/car_service/entity/Garage.java
@@ -0,0 +1,39 @@
+package com.project.car_service.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "garage")
+public class Garage {
+
+	@Id
+	@Column(name = "Address", nullable = false)
+	private String address;
+
+	@ManyToMany()
+	@JoinTable(
+			name = "workingModels",
+			joinColumns = @JoinColumn(name = "address"),
+			inverseJoinColumns = @JoinColumn(name = "carBrand")
+	)
+	private Set<CarModel> workModels;
+
+	@ManyToMany()
+	@JoinTable(
+			name = "employment",
+			joinColumns = @JoinColumn(name = "address"),
+			inverseJoinColumns = @JoinColumn(name = "PID")
+	)
+	private Set<Employee> employeeSet;
+
+	@OneToMany(mappedBy = "garage")
+	private Set<Service> services;
+}
