247976a46d8aa2f13c8e71d71e639a574ddde26a
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-10-24 21:48:25 +0300

    CarPart.java: class for entity table Car Part

---
+++ src/main/java/com/project/car_service/entity/CarPart.java
@@ -0,0 +1,25 @@
+package com.project.car_service.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "car_part")
+public class CarPart {
+
+	@Id
+	private String partName;
+
+	@OneToMany(mappedBy = "carPart")
+	private Set<Service> services;
+}
