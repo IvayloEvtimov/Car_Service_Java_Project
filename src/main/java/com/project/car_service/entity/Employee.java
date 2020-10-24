29062bf814c86df8e4eba85015526f15fa3ad296
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-10-24 21:50:34 +0300

    Employee.java: class for entity table Employee

---
+++ src/main/java/com/project/car_service/entity/Employee.java
@@ -0,0 +1,36 @@
+package com.project.car_service.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+	@Id
+	@OneToOne()
+	@JoinColumn(name = "PID")
+	@MapsId
+	private Person employee;
+
+	@Column(name = "Date_of_Hire", nullable = false)
+	private LocalDate dateOfHire;
+
+	@ManyToOne()
+	private Qualification qualification;
+
+	@ManyToMany(mappedBy = "employeeSet")
+	private Set<Garage> garageSet;
+
+	@OneToMany(mappedBy = "employee")
+	private Set<Service> services;
+
+}
