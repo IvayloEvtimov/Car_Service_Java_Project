2496179a1080ab79d538ff6d28b7885fac930663
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-10-24 21:44:55 +0300

    Car.java: class for entity table Car

---
+++ src/main/java/com/project/car_service/entity/Car.java
@@ -0,0 +1,29 @@
+package com.project.car_service.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "car")
+public class Car {
+	@Id
+	@Column(name = "license_plate")
+	private String licensePlate;
+
+	@ManyToOne()
+	private CarModel model;
+
+	@ManyToOne()
+	private Client owner;
+
+	@OneToMany(mappedBy = "car")
+	private Set<Service> services;
+
+}
