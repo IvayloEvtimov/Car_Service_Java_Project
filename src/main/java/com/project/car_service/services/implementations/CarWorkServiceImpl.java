81a3218c0b4142b39f02e53d2f10be2e272d744a
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-10-27 21:42:27 +0200

    CarWorkServiceImpl.java: Implementation of common DB queries

---
+++ src/main/java/com/project/car_service/services/implementations/CarWorkServiceImpl.java
@@ -0,0 +1,43 @@
+package com.project.car_service.services.implementations;
+
+import com.project.car_service.entity.*;
+import com.project.car_service.repository.CarWorkRepository;
+import com.project.car_service.services.CarWorkService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CarWorkServiceImpl implements CarWorkService {
+
+	private final CarWorkRepository carWorkRepository;
+
+
+	@Override
+	public List<CarWork> getServices() {
+		return carWorkRepository.findAll();
+	}
+
+	@Override
+	public CarWork getService(Long serviceId) {
+		return carWorkRepository.findById(serviceId)
+				.orElseThrow(() -> new IllegalArgumentException("Wrong service id: " + serviceId));
+	}
+
+	@Override
+	public CarWork create(CarWork carWork) {
+		return carWorkRepository.save(carWork);
+	}
+
+	@Override
+	public CarWork update(Long serviceId, CarWork carWork) {
+		return carWorkRepository.save(carWork);
+	}
+
+	@Override
+	public void deleteCar(Long serviceId) {
+		carWorkRepository.deleteById(serviceId);
+	}
+}
