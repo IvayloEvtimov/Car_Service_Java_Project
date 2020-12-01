d894862b2183da3e1b022fe02a956119e27d401e
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-01 21:40:11 +0200

    CarServiceNotFoundException.java: Added exception class for http error 404

---
+++ src/main/java/com/project/car_service/exceptions/CarServiceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.project.car_service.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class CarServiceNotFoundException extends RuntimeException{
+    public CarServiceNotFoundException(String message) { super(message); }
+}
