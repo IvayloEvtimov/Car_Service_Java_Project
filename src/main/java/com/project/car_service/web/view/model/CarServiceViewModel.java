85d76daa850abe7c6a20fbf739e9d722e550e242
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-22 15:32:05 +0200

    Create CarServiceViewModel.java
    Added View Model for displaying entity data

---
+++ src/main/java/com/project/car_service/web/view/model/CarServiceViewModel.java
@@ -0,0 +1,22 @@
+package com.project.car_service.web.view.model;
+
+import com.project.car_service.data.entity.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CarServiceViewModel {
+	private Long serviceId;
+	private Garage garage;
+	private Person employee;
+	private Person client;
+	private Car car;
+	private CarPart carPart;
+	private Qualification qualification;
+	private Date dateOfService;
+}
