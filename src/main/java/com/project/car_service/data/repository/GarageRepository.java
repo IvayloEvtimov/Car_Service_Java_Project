2f6b4c49b84f3826b4c6b50ae8018abd013e5703
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-22 15:30:33 +0200

    Create GarageRepository.java
    Repository for Garage Entity

---
+++ src/main/java/com/project/car_service/data/repository/GarageRepository.java
@@ -0,0 +1,8 @@
+package com.project.car_service.data.repository;
+
+import com.project.car_service.data.entity.Garage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GarageRepository extends JpaRepository<Garage, Long> {
+
+}
