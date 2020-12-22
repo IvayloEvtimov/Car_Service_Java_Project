07bfd380a5934b086ae6841f508c70c0b4d321b1
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-22 15:29:37 +0200

    Create PersonService.java
    Added interface for Person Entity Class

---
+++ src/main/java/com/project/car_service/services/PersonService.java
@@ -0,0 +1,9 @@
+package com.project.car_service.services;
+
+import com.project.car_service.dto.PersonDTO;
+
+import java.util.List;
+
+public interface PersonService {
+	List<PersonDTO> getPersons();
+}
