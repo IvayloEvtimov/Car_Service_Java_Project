e0c4f567607c976470744ebf1a7c069c26c4ba4f
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-22 15:30:46 +0200

    Create EmploymentRepository.java
    Repository for Employment Entity

---
+++ src/main/java/com/project/car_service/data/repository/EmploymentRepository.java
@@ -0,0 +1,12 @@
+package com.project.car_service.data.repository;
+
+import com.project.car_service.data.entity.Employment;
+import com.project.car_service.data.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface EmploymentRepository extends JpaRepository<Employment, Long> {
+
+}
