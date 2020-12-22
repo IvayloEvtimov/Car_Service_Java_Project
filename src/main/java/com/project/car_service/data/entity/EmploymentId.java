8bdb9067a4a74a1cc72038c32d75bab1c02f66aa
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-22 17:30:17 +0200

    EmploymentId.class: Added Id class for Employment entity's composite ids

---
+++ src/main/java/com/project/car_service/data/entity/EmploymentId.java
@@ -0,0 +1,8 @@
+package com.project.car_service.data.entity;
+
+import java.io.Serializable;
+
+public class EmploymentId implements Serializable {
+	private Garage garage;
+	private Person employee;
+}
