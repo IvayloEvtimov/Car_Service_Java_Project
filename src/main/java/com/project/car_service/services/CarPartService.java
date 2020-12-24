d4a89305c74ca4052d78879bf1471563cbc419d1
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-12-24 21:36:47 +0200

    CarPartService.java: Add service interface for carPart entity

---
+++ src/main/java/com/project/car_service/services/CarPartService.java
@@ -0,0 +1,9 @@
+package com.project.car_service.services;
+
+import com.project.car_service.dto.CarPartDTO;
+
+import java.util.List;
+
+public interface CarPartService {
+	List<CarPartDTO> getCarParts();
+}
