11fde92090a7de889597b16bc532ccd3c4c5dec1
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-12-24 21:55:20 +0200

    QualificationService.java: Service for entity Qualification

---
+++ src/main/java/com/project/car_service/services/QualificationService.java
@@ -0,0 +1,7 @@
+package com.project.car_service.services;
+
+import com.project.car_service.dto.QualificationDTO;
+
+public interface QualificationService {
+	List<QualificationDTO> getQualifications();
+}
