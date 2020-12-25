c321e14d20c29cdf9e052f918340e3c07065c521
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-25 23:33:44 +0200

    WorkBrandId.java: Id class for WorkBrand entity

---
+++ src/main/java/com/project/car_service/data/entity/WorkBrandId.java
@@ -0,0 +1,8 @@
+package com.project.car_service.data.entity;
+
+import java.io.Serializable;
+
+public class WorkBrandId implements Serializable {
+	private Garage garage;
+	private CarBrand carBrand;
+}
