d878c04358f4a8351d06e9134e18cb71ed96c7c2
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-25 21:09:02 +0200

    EmploymentRepository.java: Added querry for finding if Employee works in a certain Garage

--- src/main/java/com/project/car_service/data/repository/EmploymentRepository.java
+++ src/main/java/com/project/car_service/data/repository/EmploymentRepository.java
@@ -1,6 +1,7 @@
 package com.project.car_service.data.repository;
 
 import com.project.car_service.data.entity.Employment;
+import com.project.car_service.data.entity.Garage;
 import com.project.car_service.data.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,5 +9,5 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface EmploymentRepository extends JpaRepository<Employment, String> {
-
+	List<Employment> findEmploymentByEmployeeAndGarage( Person employee, Garage garage );
 }
