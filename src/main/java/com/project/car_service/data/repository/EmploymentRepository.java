1017a2220759726ce36ad2003766785456dda238
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-25 23:35:44 +0200

    EmploymentRepository.java: Added function to get emploment record based on employee and qualification

--- src/main/java/com/project/car_service/data/repository/EmploymentRepository.java
+++ src/main/java/com/project/car_service/data/repository/EmploymentRepository.java
@@ -3,6 +3,7 @@ package com.project.car_service.data.repository;
 import com.project.car_service.data.entity.Employment;
 import com.project.car_service.data.entity.Garage;
 import com.project.car_service.data.entity.Person;
+import com.project.car_service.data.entity.Qualification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +11,6 @@ import java.util.List;
 
 public interface EmploymentRepository extends JpaRepository<Employment, String> {
 	List<Employment> findEmploymentByEmployeeAndGarage( Person employee, Garage garage );
+
+	Employment getEmploymentByEmployee_PIDAndQualification( String PID, Qualification qualification );
 }
