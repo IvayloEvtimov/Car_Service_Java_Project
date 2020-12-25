55b02477b7b43f4fe1749f6e800d2ea67b8ef7f9
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-25 15:44:40 +0200

    QualificationService.java: Added List class import

--- src/main/java/com/project/car_service/services/QualificationService.java
+++ src/main/java/com/project/car_service/services/QualificationService.java
@@ -2,6 +2,8 @@ package com.project.car_service.services;
 
 import com.project.car_service.dto.QualificationDTO;
 
+import java.util.List;
+
 public interface QualificationService {
 	List<QualificationDTO> getQualifications();
 }
