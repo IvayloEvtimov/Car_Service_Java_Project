0c6e18b1bafe8e52d488e58e49548a0ffd329893
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-26 17:50:08 +0200

    CreateCarServiceViewModel.java: Added function to check input

--- src/main/java/com/project/car_service/web/view/model/CreateCarServiceViewModel.java
+++ src/main/java/com/project/car_service/web/view/model/CreateCarServiceViewModel.java
@@ -1,39 +1,57 @@
 package com.project.car_service.web.view.model;
 
 import com.project.car_service.data.entity.*;
+import com.project.car_service.services.EmploymentService;
+import com.project.car_service.services.WorkBrandService;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Future;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Getter
 @Setter
 public class CreateCarServiceViewModel {
-	@NotBlank
+	@NotNull
 	private Garage garage;
 
-	@NotBlank
+	@NotNull
 	private Person employee;
 
-	@NotBlank
+	@NotNull
 	private Person client;
 
-	@NotBlank
+	@NotNull
 	private Car car;
 
-	@NotBlank
+	@NotNull
 	private CarPart carPart;
 
-	@NotBlank
+	@NotNull
 	private Qualification qualification;
 
 	@NotNull
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@Future(message = "Date must be in the future")
 	private Date dateOfService;
+
+	private Integer price;
+
+	public boolean check( EmploymentService employmentService, WorkBrandService workBrandService ) {
+		if (car.getOwner() != client)
+			return false;
+
+		if (employmentService.findEmploymentByEmployeeAndGarage(employee, garage).isEmpty())
+			return false;
+
+		if (employmentService.getEmploymentByEmployee_PIDAndQualification(employee.getPID(), qualification) == null)
+			return false;
+
+		if (workBrandService.getWorkBrandByCarBrand_BrandNameAndGarage_UIC(car.getModel().getCarBrand().getBrandName(), garage.getUIC()) == null)
+			return false;
+
+
+		return true;	}
 }
