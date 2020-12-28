9a476607119b524f037756b5274b8c5eebc08999
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-29 00:10:58 +0200

    CarServiceRepository.java: Added function query to find all Clients

--- src/main/java/com/project/car_service/data/repository/CarServiceRepository.java
+++ src/main/java/com/project/car_service/data/repository/CarServiceRepository.java
@@ -2,7 +2,9 @@ package com.project.car_service.repository;
 
 import com.project.car_service.data.entity.Car;
 import com.project.car_service.data.entity.CarService;
+import com.project.car_service.data.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -20,4 +22,7 @@ public interface CarServiceRepository extends JpaRepository<CarService, Long> {
 	List<CarService> findCarServicesByCar_LicensePlateAndEmployee_PID(String licensePlate, String PID);
 
 	List<CarService> findCarServicesByClient_PID(String PID);
+
+	@Query("SELECT DISTINCT cs.client FROM CarService cs")
+	List<Person> findAllClients();
 }
