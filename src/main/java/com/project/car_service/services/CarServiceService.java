028a234ffe6f0b97a4136d7945e8886e74b46ee2
Ivo Evtimov <ivoevtimov97@abv.bg>
2020-12-29 02:40:17 +0200

    Update CarServiceService.java

--- src/main/java/com/project/car_service/services/CarServiceService.java
+++ src/main/java/com/project/car_service/services/CarServiceService.java
@@ -8,6 +8,8 @@ import com.project.car_service.dto.UpdateCarServiceDTO;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
+import java.util.AbstractMap;
+import java.util.HashSet;
 import java.util.List;
 
 public interface CarServiceService {
@@ -22,14 +24,32 @@ public interface CarServiceService {
 
 	void deleteService( Long serviceId );
 
-	List<CarServiceDTO> findCarServicesByCar_LicensePlate(String licensePlate);
+	List<CarServiceDTO> findCarServicesByCar_LicensePlate( String licensePlate );
 
-	List<CarServiceDTO> findCarServicesByEmployee_PID(String PID);
+	List<CarServiceDTO> findCarServicesByEmployee_PID( String PID );
 
 //	List<PersonDTO> findCarServicesByCar_LicensePlateAndEmployee_PID( String licensePlate, String PID);
 
-	List<CarServiceDTO> findCarServicesByClient_PID(String PID);
+	List<CarServiceDTO> findCarServicesByClient_PID( String PID );
 
 	List<PersonDTO> findAllClients();
+
+	List<String> findAllBrands();
+
+	List<Integer> findAllCarYears();
+
+	List<String> findAllQualifications();
+
+	Integer countAllByCar_Model_CarBrand_BrandName( String brand );
+
+	Integer countAllByCar_Model_Year( Integer year );
+
+	Integer countAllByQualification_QualificationName( String name );
+
+	HashSet<AbstractMap.SimpleEntry<String, Integer>> countCarBrands();
+
+	HashSet<AbstractMap.SimpleEntry<Integer, Integer>> countCarYears();
+
+	HashSet<AbstractMap.SimpleEntry<String, Integer>> countQualifications();
 
 }
