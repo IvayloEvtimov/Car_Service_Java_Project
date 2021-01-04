6139663f25a8b6670df0355e2531ea027d4400c7
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-01-04 17:25:21 +0200

    Service and Implementation for entity User

---
+++ src/main/java/com/project/car_service/services/UserService.java
@@ -0,0 +1,8 @@
+package com.project.car_service.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
+public interface UserService extends UserDetailsService {
+
+}
