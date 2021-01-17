1a575dfaf4d0fec4b1a787d0c3099c89da80bd59
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-01-17 19:14:30 +0200

    Update IndexController.java

--- src/main/java/com/project/car_service/web/view/controllers/IndexController.java
+++ src/main/java/com/project/car_service/web/view/controllers/IndexController.java
@@ -1,10 +1,14 @@
 package com.project.car_service.web.view.controllers;
 
+import com.project.car_service.data.entity.User;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @AllArgsConstructor
@@ -12,9 +16,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @GetMapping
-    public String getIndex(Model model) {
+    public Object getIndex( Model model) {
         final String welcomeMessage = "Welcome";
-        model.addAttribute("welcome",welcomeMessage);
-        return "index";
+//        model.addAttribute("welcome",welcomeMessage);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//      model.addAttribute("username", authentication.getName());
+
+        if(authentication.getPrincipal() == "anonymousUser") {
+            return new RedirectView("login");
+        }else {
+            User principal = (User) authentication.getPrincipal();
+            model.addAttribute("username", principal.getUsername());
+
+            return "index";
+        }
+
+    }
+
+    @GetMapping("login")
+    public String login(Model model) {
+        final String welcomeMessage = "Welcome to Car Service System!";
+        model.addAttribute("welcome", welcomeMessage);
+        return "login";
+    }
+
+    @GetMapping("unauthorized")
+    public String unauthorized(Model model) {
+        final String welcomeMessage = "Welcome to the Car Service System!";
+        model.addAttribute("welcome", welcomeMessage);
+        return "unauthorized";
     }
 }
