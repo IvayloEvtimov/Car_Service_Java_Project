058c0a39e11a172c6221ed6b397c171d2bd697a7
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-01-17 18:58:56 +0200

    Create CarServiceApplicationTest.java

---
+++ src/test/java/com/project/car_service/CarServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package com.project.car_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
