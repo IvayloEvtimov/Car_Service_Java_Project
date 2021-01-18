fc2b721fa8e93977778f8fcde962765cadda70b8
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2021-01-18 22:49:45 +0200

    Create CreatePersonDTO.java

---
+++ src/main/java/com/project/car_service/dto/CreatePersonDTO.java
@@ -0,0 +1,27 @@
+package com.project.car_service.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class CreatePersonDTO {
+	@NotNull
+	public String PID;
+
+	@NotNull
+	public String firstName;
+
+	@NotNull
+	public String lastName;
+
+	@NotNull
+	public LocalDate dateOfBirth;
+}
