2f65ed4eb219eea00ad862d88356dbabb327028f
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-01-19 01:07:58 +0200

    GarageService.java; Added function for create, edit,  delete, find

--- src/main/java/com/project/car_service/services/GarageService.java
+++ src/main/java/com/project/car_service/services/GarageService.java
@@ -1,9 +1,21 @@
 package com.project.car_service.services;
 
+import com.project.car_service.data.entity.Garage;
+import com.project.car_service.dto.CreateGarageDTO;
 import com.project.car_service.dto.GarageDTO;
+import com.project.car_service.dto.UpdateGarageDTO;
 
+import javax.validation.Valid;
 import java.util.List;
 
 public interface GarageService {
 	List<GarageDTO> getGarages();
+
+	Garage createGarage(@Valid CreateGarageDTO createGarageDTO);
+
+	Garage updateGarage(String UIC, UpdateGarageDTO updateGarageDTO);
+
+	void deleteGarage(String UIC);
+
+	GarageDTO findByUIC(String UIC);
 }
