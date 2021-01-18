c26000dbba079b123dc40a05082c6e5178c65349
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-01-19 01:06:36 +0200

    Create CreateGarageViewModel.java

---
+++ src/main/java/com/project/car_service/web/view/model/CreateGarageViewModel.java
@@ -0,0 +1,12 @@
+package com.project.car_service.web.view.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CreateGarageViewModel {
+	private String UIC;
+	private String name;
+	private String address;
+}
