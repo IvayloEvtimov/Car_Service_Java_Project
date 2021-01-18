25e4cf3331e7a241a201c0a84e9223ec6f230526
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2021-01-18 22:50:16 +0200

    Create UpdatePersonViewModel.java

---
+++ src/main/java/com/project/car_service/web/view/model/UpdatePersonViewModel.java
@@ -0,0 +1,18 @@
+package com.project.car_service.web.view.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UpdatePersonViewModel {
+	private String PID;
+	private String firstName;
+	private String lastName;
+	private LocalDate dateOfBirth;
+}
+
