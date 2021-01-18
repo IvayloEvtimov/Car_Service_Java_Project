c0072b82d13f96094c5289272e0639e09967e5f5
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2021-01-18 22:50:41 +0200

    PersonService.java: Added save, update, delete functions

--- src/main/java/com/project/car_service/services/PersonService.java
+++ src/main/java/com/project/car_service/services/PersonService.java
@@ -1,9 +1,20 @@
 package com.project.car_service.services;
 
+import com.project.car_service.data.entity.Person;
+import com.project.car_service.dto.CreatePersonDTO;
 import com.project.car_service.dto.PersonDTO;
+import com.project.car_service.dto.UpdatePersonDTO;
+import com.project.car_service.web.view.model.CreatePersonViewModel;
 
+import javax.validation.Valid;
 import java.util.List;
 
 public interface PersonService {
 	List<PersonDTO> getPersons();
+
+	Person createPerson( @Valid CreatePersonDTO createPersonDTO );
+
+	Person updatePerson( String PID, UpdatePersonDTO updatePersonDTO );
+
+	void deletePerson( String PID );
 }
