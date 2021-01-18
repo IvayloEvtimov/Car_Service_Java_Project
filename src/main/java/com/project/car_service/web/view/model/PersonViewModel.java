32d651d7c9031458b54f8611a99c06bfb9dabcb4
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2021-01-18 22:50:12 +0200

    Create PersonViewModel.java

---
+++ src/main/java/com/project/car_service/web/view/model/PersonViewModel.java
@@ -0,0 +1,17 @@
+package com.project.car_service.web.view.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PersonViewModel {
+	private String PID;
+	private String firstName;
+	private String lastName;
+	private LocalDate dateOfBirth;
+}
