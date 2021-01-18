5874713dde23fa976020acff3fc55b25145eb719
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-01-19 01:08:22 +0200

    GarageServiceImpl.java: Added function implementation for create, edit, delete, find

--- src/main/java/com/project/car_service/services/implementations/GarageServiceImpl.java
+++ src/main/java/com/project/car_service/services/implementations/GarageServiceImpl.java
@@ -2,13 +2,19 @@ package com.project.car_service.services.implementations;
 
 import com.project.car_service.data.entity.Garage;
 import com.project.car_service.data.repository.GarageRepository;
+import com.project.car_service.dto.CreateGarageDTO;
 import com.project.car_service.dto.GarageDTO;
+import com.project.car_service.dto.PersonDTO;
+import com.project.car_service.dto.UpdateGarageDTO;
+import com.project.car_service.exceptions.GarageNotFoundException;
+import com.project.car_service.exceptions.PersonNotFoundException;
 import com.project.car_service.services.GarageService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,8 +34,28 @@ public class GarageServiceImpl implements GarageService {
 				.collect(Collectors.toList());
 	}
 
+	@Override
+	public Garage createGarage(@Valid CreateGarageDTO createGarageDTO) {
+		return garageRepository.save(modelMapper.map(createGarageDTO, Garage.class));
+	}
 
-	private GarageDTO convertToGarageDTO( Garage garage ) {
+	@Override
+	public Garage updateGarage(String UIC, UpdateGarageDTO updateGarageDTO) {
+		return garageRepository.save(modelMapper.map(updateGarageDTO, Garage.class));
+	}
+
+	@Override
+	public void deleteGarage(String UIC) {
+		garageRepository.deleteById(UIC);
+	}
+
+	@Override
+	public GarageDTO findByUIC(String UIC) {
+		return modelMapper.map(garageRepository.findById(UIC)
+		.orElseThrow(() -> new GarageNotFoundException("Invalid Garage UIC: " + UIC)), GarageDTO.class);
+	}
+
+	private GarageDTO convertToGarageDTO(Garage garage) {
 		return modelMapper.map(garage, GarageDTO.class);
 	}
 }
