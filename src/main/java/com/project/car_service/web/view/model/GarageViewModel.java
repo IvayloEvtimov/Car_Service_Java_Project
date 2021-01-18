171b7fcb8d9c591595803afa1ba818a0a81d51c7
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-01-19 01:06:35 +0200

    Create GarageViewModel.java

---
+++ src/main/java/com/project/car_service/web/view/model/GarageViewModel.java
@@ -0,0 +1,14 @@
+package com.project.car_service.web.view.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class GarageViewModel {
+	private String UIC;
+	private String name;
+	private String address;
+}
