d95166d27bd2ecf17b3ad9ba9ba7c38adb5bfebb
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-01-19 23:07:42 +0200

    EmploymentRepository.java: Added findByGaraga_UIC function

--- src/main/java/com/project/car_service/data/repository/EmploymentRepository.java
+++ src/main/java/com/project/car_service/data/repository/EmploymentRepository.java
@@ -1,16 +1,15 @@
 package com.project.car_service.data.repository;
 
 import com.project.car_service.data.entity.Employment;
-import com.project.car_service.data.entity.Garage;
-import com.project.car_service.data.entity.Person;
 import com.project.car_service.data.entity.Qualification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface EmploymentRepository extends JpaRepository<Employment, String> {
 	List<Employment> findEmploymentByEmployee_PIDAndGarage_UIC(String PID, String UIC);
 
-	Employment getEmploymentByEmployee_PIDAndQualification( String PID, Qualification qualification );
+	Employment getEmploymentByEmployee_PIDAndQualification(String PID, Qualification qualification);
+
+	List<Employment> findAllByGarage_UIC(String UIC);
 }
