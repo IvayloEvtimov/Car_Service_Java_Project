76f2e42bd939f90f7392cfda7e5cc0d9bc43f280
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-01-20 23:44:09 +0200

    Update CreatePersonViewModel.java

--- src/main/java/com/project/car_service/web/view/model/CreatePersonViewModel.java
+++ src/main/java/com/project/car_service/web/view/model/CreatePersonViewModel.java
@@ -3,15 +3,27 @@ package com.project.car_service.web.view.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class CreatePersonViewModel {
+	@NotNull
 	private String PID;
+
+	@NotNull
 	private String firstName;
+
+	@NotNull
 	private String lastName;
+
+	@NotNull
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Future(message = "Date must be in the future")
 	private LocalDate dateOfBirth;
 }
