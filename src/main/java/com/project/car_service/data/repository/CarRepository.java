25714de1da732ec2ee3dbdad30c88c942fb07918
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-01-20 08:37:47 +0200

    CarRepository.java: Added findAllByOwner_PID function

--- src/main/java/com/project/car_service/data/repository/CarRepository.java
+++ src/main/java/com/project/car_service/data/repository/CarRepository.java
@@ -1,10 +1,10 @@
 package com.project.car_service.data.repository;
 
 import com.project.car_service.data.entity.Car;
-import com.project.car_service.dto.CarDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, String> {
+	List<Car> findAllByOwner_PID(String PID);
 }
