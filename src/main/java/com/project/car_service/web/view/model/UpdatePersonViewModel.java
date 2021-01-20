594b30b2ff613f52a5a1b25106c96e9d919644c9
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-01-20 23:44:13 +0200

    Update UpdatePersonViewModel.java

--- src/main/java/com/project/car_service/web/view/model/UpdatePersonViewModel.java
+++ src/main/java/com/project/car_service/web/view/model/UpdatePersonViewModel.java
@@ -3,16 +3,28 @@ package com.project.car_service.web.view.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class UpdatePersonViewModel {
+
+	@NotBlank
 	private String PID;
+
+	@NotBlank
 	private String firstName;
+
+	@NotBlank
 	private String lastName;
+
+	@NotNull
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate dateOfBirth;
 }
 
